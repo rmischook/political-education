b241bb7942f19a68b7d7a2023108a68ddd21b765
Richard Mischook <rmischook@gcdtech.com>
2020-07-15 17:48:53 +0100

    Build files

---
+++ build/precache-manifest.8bdd6f32ac0dbee893df5cedca354835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add5b0f3ade083a379fc3c71cac43d21",
+    "url": "/political-education/index.html"
+  },
+  {
+    "revision": "3887a94dd103c9f0d266",
+    "url": "/political-education/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/political-education/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/political-education/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3887a94dd103c9f0d266",
+    "url": "/political-education/static/js/main.62ff1784.chunk.js"
+  },
+  {
+    "revision": "f352f79256e17787a681",
+    "url": "/political-education/static/js/runtime-main.35f1c08c.js"
+  }
+]);
